cd63a7abc6bf3f703b2f10c50d62aaead42fccb7
EricNaKor <122855875+EricNakor@users.noreply.github.com>
2024-01-21 16:22:44 +0900

    Initial commit

---
+++ src/main/java/com/Duo960118/fitow/FitowApplication.java
@@ -0,0 +1,13 @@
+package com.Duo960118.fitow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Duo960118/fitow/FitowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Duo960118.fitow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
